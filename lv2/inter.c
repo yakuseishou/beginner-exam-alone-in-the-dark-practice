a7e18449f6a43ef3e22ecdd2075ab524126fb8de
Kai Chen <kchen2@e1z2r3p28.42.us.org>
2019-04-24 15:09:03 -0700

    inter

--- lv2/inter.c
+++ lv2/inter.c
@@ -3,55 +3,44 @@
 /*                                                        :::      ::::::::   */
 /*   inter.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kchen2 <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: kchen2 <kchen2@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/03/12 13:40:31 by kchen2            #+#    #+#             */
-/*   Updated: 2019/03/12 15:12:13 by kchen2           ###   ########.fr       */
+/*   Created: 2019/04/24 14:58:31 by kchen2            #+#    #+#             */
+/*   Updated: 2019/04/24 15:08:15 by kchen2           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-int		checkd(char *s, char c, int n)
+int		check(char *s, char c, int n)
 {
 	int		i;
 
-	i = 0;
-	while (i < n)
-	{
+	i = -1;
+	while(++i < n)
 		if (s[i] == c)
 			return (0);
-		i++;
-	}
 	return(1);
 }
 
-void	inter(char *s, char *s2)
+void	inn(char *s1, char *s2)
 {
 	int		i;
 	int		j;
 
-	i = 0;
+	i = -1;
 	j = 0;
-	while (s2[j])
-	{
-		if ((checkd(s2, s2[j], j) == 1) && !(checkd(s2, s[i], j)))
-		{
-			while(s[i])
-   		 	{
-       			 if (checkd(s, s[i], i) == 1)		
-					write(1, &s[i], 1);
-				i++;
-			}
-		}
+	while(s2[j])
 		j++;
-	}
+	while(s1[++i])
+		if ((check(s1, s1[i], i) == 1) && (!(check(s2, s1[i], j))))
+			write(1, &s1[i], 1);
 }
 
 int		main(int ac, char **av)
 {
 	if (ac == 3)
-		inter(av[1], av[2]);
+		inn(av[1], av[2]);
 	write(1, "\n", 1);
 	return (0);
 }
