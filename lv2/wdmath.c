74c7c3c65db9b0da46d2ad53c3ea4399333f8590
Kai Chen <kchen2@e1z2r3p28.42.us.org>
2019-04-24 18:35:22 -0700

    wdmath

--- lv2/wdmath.c
+++ lv2/wdmath.c
@@ -3,49 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   wdmath.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kchen2 <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: kchen2 <kchen2@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/12 20:44:12 by kchen2            #+#    #+#             */
-/*   Updated: 2019/03/12 20:48:33 by kchen2           ###   ########.fr       */
+/*   Updated: 2019/04/24 18:34:09 by kchen2           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_putstr(char *s)
-{
-	int		i;
-
-	i = 0;
-	while (s[i])
-	{
-		write(1, &s[i], 1);
-		i++;
-	}
-}
-
-int		checkd(char *s, char c, int i)
-{
-	if (s[i] == c)
-		return (1);
-	return (0);
-}
-
-void	wdm(char *s, char *s2)
+void	wdm(char *s1, char *s2)
 {
 	int		i;
 	int		j;
 
 	i = 0;
 	j = 0;
-	while (s[i] && s2[j])
+	while(s2[j] && s1[i])
 	{
-		if (checkd(s2, s[i], j) == 1)
+		if (s2[j] == s1[i])
 			i++;
 		j++;
 	}
-	if (s[i] == '\0')
-		ft_putstr(s);
+	if (s1[i] == '\0')
+		write(1, s1, i);
 }
 
 int		main(int ac, char **av)
